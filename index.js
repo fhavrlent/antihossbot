9c7b0aa0fd528344a7f17ab6dcbfe67c0e66a3d7
Filip Havrlent <filip@havrlent.com>
2021-09-12 14:08:00 +0200

    Refactoring and eslint config

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const tmi = require("tmi.js");
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const crypto = require("crypto");
-require("dotenv").config();
+/* eslint-disable no-console */
+/* eslint-disable camelcase */
+const tmi = require('tmi.js');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const crypto = require('crypto');
+const http = require('http');
+
+require('dotenv').config();
 
 const {
   CHANNEL: channel,
   USERNAME: username,
   PASSWORD: password,
   PORT: port = 8080,
-  HOOK_SECRET: hook_secret,
+  HOOK_SECRET: hookSecret,
 } = process.env;
 
 const options = {
@@ -28,121 +32,128 @@ const options = {
   channels: [channel],
 };
 
-let client = new tmi.client(options);
+const client = new tmi.Client(options);
 
-// Connect the client to the server..
 client.connect();
 
-client.on("connected", () => {
-  // Express basics
+client.on('connected', () => {
   const app = express();
-  const http = require("http").Server(app);
-  http.listen(port, function () {
-    console.log("Server raised on", port);
+  const server = http.createServer(app);
+
+  server.listen(port, () => {
+    console.log('Server raised on', port);
   });
+
+  const isHossFollow = (subscription, event) =>
+    subscription?.type === 'channel.follow' &&
+    (event?.user_name.toLowerCase().startsWith('hoss') ||
+      event?.user_name.toLowerCase().startsWith('host'));
+
+  const banBot = (event) => client.say(channel, `/ban ${event.user_name}`);
+
+  const writeLogs = (body, headers) => {
+    fs.appendFileSync(
+      path.join(__dirname, 'webhooks.log'),
+      `${JSON.stringify({
+        body,
+        headers,
+      })}\n`
+    );
+    fs.appendFileSync(
+      path.join(__dirname, 'last_webhooks.log'),
+      JSON.stringify(
+        {
+          body,
+          headers,
+        },
+        null,
+        4
+      )
+    );
+  };
 
   app.use(
     express.json({
-      verify: function (req, res, buf, encoding) {
+      verify(req, _, buf) {
         req.twitch_eventsub = false;
         if (
           req.headers &&
-          req.headers.hasOwnProperty("twitch-eventsub-message-signature")
+          Object.prototype.hasOwnProperty.call(
+            req.headers,
+            'twitch-eventsub-message-signature'
+          )
         ) {
           req.twitch_eventsub = true;
 
-          let id = req.headers["twitch-eventsub-message-id"];
-          let timestamp = req.headers["twitch-eventsub-message-timestamp"];
-          let [algo, signature] =
-            req.headers["twitch-eventsub-message-signature"].split("=");
+          const id = req.headers['twitch-eventsub-message-id'];
+          const timestamp = req.headers['twitch-eventsub-message-timestamp'];
+          const [, signature] =
+            req.headers['twitch-eventsub-message-signature'].split('=');
 
           req.twitch_hex = crypto
-            .createHmac("sha256", hook_secret)
+            .createHmac('sha256', hookSecret)
             .update(id + timestamp + buf)
-            .digest("hex");
+            .digest('hex');
           req.twitch_signature = signature;
 
-          if (req.twitch_signature != req.twitch_hex) {
-            console.error("Signature Mismatch");
-          } else {
-            console.log("Signature OK");
-          }
+          console.log(
+            req.twitch_signature !== req.twitch_hex
+              ? 'Signature Mismatch'
+              : 'Signature OK'
+          );
         }
       },
     })
   );
 
   app
-    .route("/")
-    .get((req, res) => {
-      console.log("Incoming Get request on /");
-      res.send("There is no GET Handler");
+    .route('/')
+    .get((_, res) => {
+      console.log('Incoming Get request on /');
+      res.send('Hello!');
     })
     .post((req, res) => {
-      console.log("Incoming Post request on /", req.body);
+      console.log('Incoming Post request on /', req.body);
 
-      const {subscription, event} = req.body
-      if (subscription?.type === "channel.follow") {
-        if (
-          event?.user_name.toLowerCase().startsWith("hoss") ||
-          event?.user_name.toLowerCase().startsWith("host")
-        ) {
-          client.say(channel, `/ban ${event.user_name}`);
-        }
-      }
+      const { body, headers, twitch_hex, twitch_eventsub, twitch_signature } =
+        req;
+      const { subscription, event, challenge } = body;
 
-      if (req.twitch_eventsub) {
-        if (
-          req.headers["twitch-eventsub-message-type"] ==
-          "webhook_callback_verification"
-        ) {
-          if (req.body.hasOwnProperty("challenge")) {
-            if (req.twitch_hex == req.twitch_signature) {
-              console.log("Got a challenge, return the challenge");
-              res.send(encodeURIComponent(req.body.challenge));
-              return;
+      if (isHossFollow(subscription, event)) banBot(event);
+
+      if (twitch_eventsub) {
+        switch (headers['twitch-eventsub-message-type']) {
+          case 'webhook_callback_verification':
+            if (Object.prototype.hasOwnProperty.call(body, 'challenge')) {
+              if (twitch_hex === twitch_signature) {
+                console.log('Got a challenge, return the challenge');
+                res.send(encodeURIComponent(challenge));
+                return;
+              }
             }
-          }
-          res.status(403).send("Denied");
-        } else if (
-          req.headers["twitch-eventsub-message-type"] == "revocation"
-        ) {
-          res.send("Ok");
-        } else if (
-          req.headers["twitch-eventsub-message-type"] == "notification"
-        ) {
-          if (req.twitch_hex == req.twitch_signature) {
-            console.log("The signature matched");
-            res.send("Ok");
-            fs.appendFileSync(
-              path.join(__dirname, "webhooks.log"),
-              JSON.stringify({
-                body: req.body,
-                headers: req.headers,
-              }) + "\n"
-            );
-            fs.appendFileSync(
-              path.join(__dirname, "last_webhooks.log"),
-              JSON.stringify(
-                {
-                  body: req.body,
-                  headers: req.headers,
-                },
-                null,
-                4
-              )
-            );
-          } else {
-            console.log("The Signature did not match");
-            res.send("Ok");
-          }
-        } else {
-          console.log("Invalid hook sent to me");
-          res.send("Ok");
+            res.status(403).send('Denied');
+            break;
+          case 'revocation':
+            res.send('Ok');
+            break;
+          case 'notification':
+            if (twitch_hex === twitch_signature) {
+              console.log('The signature matched');
+              res.send('Ok');
+              writeLogs(body, headers);
+            } else {
+              console.log('The Signature did not match');
+              res.send('Ok');
+            }
+            break;
+          default:
+            console.log('Invalid hook sent to me');
+            res.send('Ok');
+            break;
         }
       } else {
         console.log("It didn't seem to be a Twitch Hook");
-        res.send("Ok");
+        res.send('Ok');
       }
     });
 });
